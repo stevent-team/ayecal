dd2c3877a738245bf3f1d264dc054250dfd3e87f
Ewan Breakey <ebreakey@gmail.com>
2022-03-07 16:33:13 +1100

    Add zulu time indicator to timestamp fields

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,6 +1,9 @@
-const formatDate = date => date
-  .toISOString()
-  .replace(/\..*/, '')
-  .replace(/[^a-zA-Z0-9]/g, '')
+const formatDate = date => {
+  const iso = date
+    .toISOString()
+    .replace(/\..*/, '')
+    .replace(/[^a-zA-Z0-9]/g, '')
+  return `${iso}Z`
+}
 
 export default formatDate
