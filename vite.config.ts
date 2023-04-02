79714c25f37e5e8fcf0164f030597f3df9ad89e4
Ben Grant <benjamin.grantGRA0007@gmail.com>
2023-04-02 22:53:38 +1000

    Set up vite config

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    })
+  ],
+  resolve: {
+    alias: {
+      '@stevent-team/ayecal': resolve(__dirname, '/src/index.ts')
+    }
+  },
+  build: {
+    ...mode !== 'demo' && { lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'ayecal',
+      fileName: 'ayecal',
+    }},
+    rollupOptions: {
+      output: {
+        exports: 'named',
+      },
+    },
+  }
+}))
