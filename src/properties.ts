2de10a2412cb584182893b28e5803b9cc60ff079
Ewan Breakey <ebreakey@gmail.com>
2023-08-21 17:03:24 +1000

    Use module rather than declaration for properties

--- src/properties.ts
+++ src/properties.ts
@@ -7,7 +7,7 @@ import type { LiteralUnion } from 'type-fest'
  *
  * @see `CALSCALE` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.7.1 | RFC 5545}
  */
-type CalendarScale = LiteralUnion<'GREGORIAN', string>
+export type CalendarScale = LiteralUnion<'GREGORIAN', string>
 
 /**
  * The method associated with this calendar.
@@ -23,7 +23,7 @@ type CalendarScale = LiteralUnion<'GREGORIAN', string>
  * @see `METHOD` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.7.2 | RFC 5545}
  * @see Specification of methods in {@link https://datatracker.ietf.org/doc/html/rfc5546 | RFC 5546}
  */
-type CalendarMethod = LiteralUnion<'PUBLISH' | 'REQUEST' | 'REPLY' | 'ADD' | 'CANCEL' | 'REFRESH' | 'COUNTER' | 'DECLINECOUNTER', string>
+export type CalendarMethod = LiteralUnion<'PUBLISH' | 'REQUEST' | 'REPLY' | 'ADD' | 'CANCEL' | 'REFRESH' | 'COUNTER' | 'DECLINECOUNTER', string>
 
 /**
  * The ID of the product that created this calendar.
@@ -34,14 +34,14 @@ type CalendarMethod = LiteralUnion<'PUBLISH' | 'REQUEST' | 'REPLY' | 'ADD' | 'CA
  *
  * @see `PRODID` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.7.3 | RFC 5545}
  */
-type CalendarProductId = string
+export type CalendarProductId = string
 
 /**
  * The name of the calendar. This is a non-standard property and sets `X-WR-CALNAME`.
  *
  * @see Non-Standard Properties in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.8.2 | RFC 5545}
  */
-type CalendarName = string
+export type CalendarName = string
 
 /**
  * The timezone of the whole calendar. This is a non-standard property and sets `X-WR-TIMEZONE`.
@@ -57,7 +57,7 @@ type CalendarName = string
  *
  * @see Non-Standard Properties in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.8.2 | RFC 5545}
  */
-type CalendarTimeZone = string
+export type CalendarTimeZone = string
 
 /**
  * Scope to use for all IDs inside this calendar.
@@ -69,28 +69,28 @@ type CalendarTimeZone = string
  *
  * @see `UID` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.4.7 | RFC 5545}
  */
-type CalendarScope = string
+export type CalendarScope = string
 
 /**
  * A short summary or subject for this component. Also known as a title for events.
  *
  * @see `SUMMARY` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.1.12 | RFC 5545}
  */
-type ComponentSummary = string
+export type ComponentSummary = string
 
 /**
  * When this component begins.
  *
  * @see `DTSTART` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.2.4 | RFC 5545}
  */
-type ComponentStartTime = Date
+export type ComponentStartTime = Date
 
 /**
  * When this component ends.
  *
  * @see `DTEND` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.2.2 | RFC 5545}
  */
-type ComponentEndTime = Date
+export type ComponentEndTime = Date
 
 /**
  * The unique identifier for this component.
@@ -102,28 +102,28 @@ type ComponentEndTime = Date
  *
  * @see `UID` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.4.7 | RFC 5545}
  */
-type ComponentId = string
+export type ComponentId = string
 
 /**
  * A detailed description for this component.
  *
  * @see `DESCRIPTION` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.1.5 | RFC 5545}
  */
-type ComponentDescription = string
+export type ComponentDescription = string
 
 /**
  * When this component was created.
  *
  * @see `CREATED` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.7.1 | RFC 5545}
  */
-type ComponentCreatedTime = Date
+export type ComponentCreatedTime = Date
 
 /**
  * When this component was last updated.
  *
  * @see `LAST-MODIFIED` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.7.3 | RFC 5545}
  */
-type ComponentUpdatedTime = Date
+export type ComponentUpdatedTime = Date
 
 /**
  * The revision number of this component, starting at 0 from when the component is first created.
@@ -132,21 +132,21 @@ type ComponentUpdatedTime = Date
  *
  * @see `SEQUENCE` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.7.4 | RFC 5545}
  */
-type ComponentRevision = number
+export type ComponentRevision = number
 
 /**
  * The location/venue for the activity defined by this component.
  *
  * @see `LOCATION` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.1.7 | RFC 5545}
  */
-type ComponentLocation = string
+export type ComponentLocation = string
 
 /**
  * The status of this event.
  *
  * @see `STATUS` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.1.11 | RFC 5545}
  */
-type EventStatus = 'TENTATIVE' | 'CONFIRMED' | 'CANCELLED'
+export type EventStatus = 'TENTATIVE' | 'CONFIRMED' | 'CANCELLED'
 
 /**
  * Whether or not this event appears to consume time on a calendar.
@@ -156,4 +156,4 @@ type EventStatus = 'TENTATIVE' | 'CONFIRMED' | 'CANCELLED'
  *
  * @see `TRANSP` in {@link https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.2.7 | RFC 5545}
  */
-type EventBusy = boolean
+export type EventBusy = boolean
